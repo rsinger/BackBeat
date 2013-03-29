2e37d61ba0b73cf0e9b856b819ac29af580d2e1c
Ross Singer <rossfsinger@gmail.com>
2013-03-29 15:14:18 -0400

    Needed to run on the proper port

--- app.js
+++ app.js
@@ -55,5 +55,5 @@ app.get('/:artist', function(req,res){
   });
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000)
+console.log('Backbeat started');
